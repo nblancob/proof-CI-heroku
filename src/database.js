6950f0f29b53349b0ac7b0adbad9e15f356fb274
Nicolás Blanco <67393704+nblancob@users.noreply.github.com>
2021-12-15 23:05:18 -0500

    Update database.js

--- src/database.js
+++ src/database.js
@@ -8,10 +8,10 @@ async function connectDB() {
   let client;
   if (connection) return connection;
   try {
-    client = await MongoClient.connect(DB_URL, {
+    client = await MongoClient.connect("mongodb+srv://student_00:estudiante2021@sandbox.m6mtx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
       useNewUrlParser: true,
     });
-    connection = client.db(DB_NAME);
+    connection = client.db("meta");
     console.log("Connected to DB");
   } catch (error) {
     console.log(error);
